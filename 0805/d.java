83d4ad425c7bd4a4df71914841061afc2c6144d8
Dudutingt <justindu1217@gmail.com>
2025-08-05 10:07:42 +0800

    0805

---
+++ 0805/d.java
@@ -0,0 +1,7 @@
+public class d {
+    public static void main(String[] args) {
+        System.out.println("# 113tkuds_2\n");
+        System.out.println("## 0805");
+        System.out.println("新增 githubdesktop 截圖");
+    }
+}
